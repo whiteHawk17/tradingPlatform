81ae9c749bf80d7d83c3e10f1c9197b9875c7b86
VinayJangra <149719059+whiteHawk17@users.noreply.github.com>
2025-06-18 22:50:14 +0530

    Update Dashboard.js

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -4,7 +4,7 @@ const Dashboard = () => {
   return (
     <div className="dashboard-main-content">
       <h2>Welcome to your Dashboard!</h2>
-      <p>This is the main content area. Your WatchList is correctly displayed on the left sidebar.</p>
+      
     </div>
   );
 };
